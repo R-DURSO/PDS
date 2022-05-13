e29c824c545c490a50d9cab60d97daf8ef6e7a93
redouanedebart <77845511+redouanedebart@users.noreply.github.com>
2022-05-13 15:15:28 +0200

    Create serial_comm.py
    python file for serial communication testing with the arm

---
+++ software/core/serial/serial_comm.py
@@ -0,0 +1,37 @@
+import serial
+import time
+
+port = "/dev/ttyACM0"
+baudrate = 9600
+
+arduino = serial.Serial(port=port,
+                        baudrate=baudrate,
+                        bytesize=serial.EIGHTBITS,
+
+                        stopbits=serial.STOPBITS_ONE,
+                        timeout=5)
+
+
+def send(msg):
+
+    if arduino.isOpen():
+        print("port already opened \n")
+    else:
+        arduino.open()
+
+    msg = msg.encode()
+    arduino.write(msg)
+    time.sleep(1)
+    data = b''
+    while(arduino.in_waiting<=0):
+        time.sleep(0.2)
+    while(arduino.in_waiting>0):
+        data += arduino.readline()
+    return data.decode()
+
+
+time.sleep(8)
+print(send("2"))
+print(send("3"))
+print(send("1"))
+#TODO: find a way to catch last message just before the end of the prog.
