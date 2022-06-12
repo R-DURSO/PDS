77dd997c9210a662c13c5b942013e0b17d26b649
R-DURSO <rdurso@outlook.fr>
2022-06-12 23:22:59 +0200

    préparation démoé

--- software/core/frame/keypoint.py
+++ software/core/frame/keypoint.py
@@ -11,17 +11,21 @@ path = os.path.dirname(__file__)
 # frame = cv.imread( path + "/triangle.jpg")
 # frame = cv.imread( path + "/carrer_blanc.jpg")
 # frame = cv.imread(path + "/boule.jpg")
-frame = cv.imread(path +"/b_tri.jpg")
-seuil = 120 
+frame = cv.imread(path +"/cube.jpg")
+frame = cv.resize(frame,(320,240))
+cv.imshow('att',frame)
+cv.waitKey(0)
+seuil = 40 
 
 def binarisation(frame):
     greyFrame = np.zeros((len(frame),len(frame[0])))
     for i in range(len(frame)):
         for j in range(len(frame[0])):
             if (sum(frame[i][j]) / 3) > seuil:
-                greyFrame[i][j] == 0
-            else:
                 greyFrame[i][j] = 255
+            else:
+
+                greyFrame[i][j] = 0
 
     return greyFrame
 
@@ -31,6 +35,12 @@ def gaussianBlur(frame):
     return frame
 
 frame = binarisation(frame)
+
+
+# frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+# frame = cv.Canny(frame,20,50)
+# lines = cv.HoughLinesP(frame,1,np.pi/180,20,100,10)
+
 frame = scipy.ndimage.median_filter(frame,size=10)
 filterX = np.array( ([-1,0,1],[-2,0,2],[-1,0,1])) 
 filterY = np.array( ([-1,-2,-1],[0,0,0],[1,2,1])) 
@@ -46,8 +56,17 @@ frameXYmasked = sp.convolve2d(frameXY,mask,mode="same")
 framemax = frameX**2 *frameYmasked**2 + frameY**2*frameXmasked**2 - 2 * frameXY*frameXYmasked
 # framemax = frameYmasked*frameYmasked*frameXmasked*frameXmasked - frameXY - 0.05 * (frameXmasked**2 + frameYmasked**2)**2
 framemax = framemax / (frameYmasked*frameYmasked + frameXmasked*frameXmasked)
-# cv.imshow("greyscaleX", frameXmasked)
-# cv.imshow("greyscaleY", frameYmasked)
-# cv.imshow("greyscaleXY", frameXYmasked)
+cv.imshow("greyscaleX", frameXmasked)
+cv.imshow("greyscaleY", frameYmasked)
+cv.imshow("greyscaleXY", frameXYmasked)
+
+
 cv.imshow("test", framemax)
 cv.waitKey(0)
+# recontrucframe = np.zeros(frame.shape)
+
+# for coords in lines:
+#     coords = coords[0]
+#     cv.line(recontrucframe, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
+# cv.imshow('houghlines3.jpg',recontrucframe)
+# cv.waitKey(0)
